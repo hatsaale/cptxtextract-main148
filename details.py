3ea2a924920dc99b6d600db3f33947b8ca3143c9
hatsaale <kkwar7988@gmail.com>
2025-04-29 01:31:49 +0530

    Update details.py

--- details.py
+++ details.py
@@ -4,6 +4,6 @@ api_id = "28748671"
 api_hash = "f53ec7c41ce34e6d585674ed9ce6167c"
 bot_token = "7896165148:AAFKmmxHT01cbgqPcuHpbigHnGETVO4vSk4"
 auth_users = [1169394017]
-sudo_user = 1169394017
+sudo_user = [1169394017]
 log_channel = -2311732949
 txt_channel = -2311732949
